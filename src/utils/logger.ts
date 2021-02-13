085f2fbb2a6da85776e092f140edbd748e0f3477
Ally Murray <allymurray88@gmail.com>
2021-02-13 13:20:08 +0000

    Updated logger typings

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,25 +5,25 @@ const pinoLog = pino();
 
 class Logger {
   info(message: string): void;
-  info(mergingObject: Object, message: string): void;
+  info(mergingObject: Object, message?: string): void;
   info(x: never, message?: string): void {
     pinoLog.info(x, message);
   }
 
   warn(message: string): void;
-  warn(mergingObject: Object, message: string): void;
+  warn(mergingObject: Object, message?: string): void;
   warn(x: never, message?: string): void {
     pinoLog.warn(x, message);
   }
 
   error(message: string): void;
-  error(mergingObject: Object, message: string): void;
+  error(mergingObject: Object, message?: string): void;
   error(x: never, message?: string): void {
     pinoLog.error(x, message);
   }
 
   debug(message: string): void;
-  debug(mergingObject: Object, message: string): void;
+  debug(mergingObject: Object, message?: string): void;
   debug(x: never, message?: string): void {
     pinoLog.debug(x, message);
   }
